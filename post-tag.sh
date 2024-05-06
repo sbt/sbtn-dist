c4f1f39c67fd72f66efa0050a54e71611206e115
Eugene Yokota <eed3si9n@gmail.com>
2024-05-05 22:11:00 -0400

    Update post-tag

--- post-tag.sh
+++ post-tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-VER=1.8.1-M1
+VER=1.10.0
 
 rm -rf target || true
 mkdir -p target
@@ -8,9 +8,7 @@ mkdir -p target/temp
 
 BASE_URL=https://github.com/sbt/sbtn-dist/releases/download/v${VER}
 
-MAC_X86_64_URL=$BASE_URL/sbtn-x86_64-apple-darwin
-
-MAC_AARCH64_URL=$BASE_URL/sbtn-aarch64-apple-darwin
+MAC_URL=$BASE_URL/sbtn-universal-apple-darwin
 
 WINDOWS_URL=$BASE_URL/sbtn-x86_64-pc-win32.exe
 
@@ -22,33 +20,24 @@ LINUX_AARCH64_URL=$BASE_URL/sbtn-aarch64-pc-linux
 # mkdir sbtn
 # cd sbtn
 
-mkdir -p target/x86_64-apple-darwin
-mkdir -p target/aarch64-apple-darwin
+mkdir -p target/universal-apple-darwin
 mkdir -p target/x86_64-pc-linux
 mkdir -p target/aarch64-pc-linux
 mkdir -p target/x86_64-pc-win32
 
-curl -L $MAC_X86_64_URL > target/x86_64-apple-darwin/sbtn
-curl -L $MAC_AARCH64_URL > target/aarch64-apple-darwin/sbtn
+curl -L $MAC_URL > target/universal-apple-darwin/sbtn
 curl -L $LINUX_X86_64_URL > target/x86_64-pc-linux/sbtn
 curl -L $LINUX_AARCH64_URL > target/aarch64-pc-linux/sbtn
 curl -L $WINDOWS_URL > target/x86_64-pc-win32/sbtn.exe
 
 cd target
 
-cd x86_64-apple-darwin
+cd universal-apple-darwin
 chmod +x sbtn
-tar czvf sbtn-x86_64-apple-darwin-$VER.tar.gz sbtn
-mv sbtn-x86_64-apple-darwin-$VER.tar.gz ../
+tar czvf sbtn-universal-apple-darwin-$VER.tar.gz sbtn
+mv sbtn-universal-apple-darwin-$VER.tar.gz ../
 cd ../
-gpg -u 0x642ac823 --detach-sign --armor sbtn-x86_64-apple-darwin-$VER.tar.gz
-
-cd aarch64-apple-darwin
-chmod +x sbtn
-tar czvf sbtn-aarch64-apple-darwin-$VER.tar.gz sbtn
-mv sbtn-aarch64-apple-darwin-$VER.tar.gz ../
-cd ../
-gpg -u 0x642ac823 --detach-sign --armor sbtn-aarch64-apple-darwin-$VER.tar.gz
+gpg -u 0x642ac823 --detach-sign --armor sbtn-universal-apple-darwin-$VER.tar.gz
 
 cd x86_64-pc-linux
 chmod +x sbtn
